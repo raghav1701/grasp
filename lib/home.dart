dbb09a3b7d48a71952f9639d4b38050aa071a428
raghav1701 <raghavagarwal665@gmail.com>
2020-09-13 16:55:15 +0530

    text

--- lib/home.dart
+++ lib/home.dart
@@ -78,19 +78,28 @@ class SliderView extends StatelessWidget {
                   Sliderimages[x],
                   fit: BoxFit.cover,
                 ),
-              )
-
+              ),
               Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [Colors.transparent, Colors.black]
-                  )
+                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [Colors.transparent, Colors.black])),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      Slideritems[x],
+                      style: TextStyle(fontSize: 25.0, color: Colors.white),
+                    )
+                  ],
                 ),
               )
-
             ],
           ),
         ),
